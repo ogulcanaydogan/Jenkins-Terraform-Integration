e5048de2f3d7263929833e8203a8ac863ba896f3
ogulcanaydogan <ogulcanaydogan@gmail.com>
2024-04-25 02:38:46 +0000

    second commit

---
+++ main.tf
@@ -0,0 +1,26 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "5.46.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-east-1" 
+
+resource "aws_s3_bucket" "my_bucket" {
+  bucket = "ogulcanaydogan222"  
+
+  tags = {
+    Name        = "ogulcanaydogan222"
+    Environment = "Development"
+  }
+}
+
+}
+
+output "bucket_arn" {
+  value = aws_s3_bucket.my_bucket.arn
+}
